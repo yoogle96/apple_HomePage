0ed48c02daa1b9f6adba8e41d347d7893d5aa9e8
yoogle <yoogle@hong-gildong-ui-MacBook-Pro.local>
2018-06-06 14:54:32 +0900

    eee

--- js/scroll.js
+++ js/scroll.js
@@ -3,28 +3,28 @@ function fnMove(seq){
     $('html, body').animate({scrollTop : offset.top}, 400);
 }
 
-var animateHTML = function () {
-var elems,
-windowHeight
-var init = function () {
-elems = document.getElementsByClassName('hidden')
-windowHeight = window.innerHeight
-_addEventHandlers()
-}
-var _addEventHandlers = function () {
-window.addEventListener('scroll', _checkPosition)
-window.addEventListener('resize', init)
-}
-var _checkPosition = function () {
-for (var i = 0; i < elems.length; i++) {
-var posFromTop = elems[i].getBoundingClientRect().top
-if (posFromTop - windowHeight <= 0) {
-elems[i].className = elems[i].className.replace('hidden', 'fade-in-element')
-}
-}
-}
-return {
-init: init
-}
-}
-animateHTML().init()
+// var animateHTML = function () {
+// var elems,
+// windowHeight
+// var init = function () {
+// elems = document.getElementsByClassName('hidden')
+// windowHeight = window.innerHeight
+// _addEventHandlers()
+// }
+// var _addEventHandlers = function () {
+// window.addEventListener('scroll', _checkPosition)
+// window.addEventListener('resize', init)
+// }
+// var _checkPosition = function () {
+// for (var i = 0; i < elems.length; i++) {
+// var posFromTop = elems[i].getBoundingClientRect().top
+// if (posFromTop - windowHeight <= 0) {
+// elems[i].className = elems[i].className.replace('hidden', 'fade-in-element')
+// }
+// }
+// }
+// return {
+// init: init
+// }
+// }
+// animateHTML().init()
